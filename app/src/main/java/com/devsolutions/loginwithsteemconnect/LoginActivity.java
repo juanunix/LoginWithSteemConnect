e4b1477ad2104d7086c93bc669d8070c22e62e2b
Juan <juanunix@gmail.com>
2018-07-23 01:05:55 -0400

    Update All

---
+++ app/src/main/java/com/devsolutions/loginwithsteemconnect/LoginActivity.java
@@ -0,0 +1,26 @@
+package com.devsolutions.loginwithsteemconnect;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        if(getIntent()!=null) {
+            Uri uri = getIntent().getData();
+            if (uri != null) {
+                Log.d("MainAtv-receivedata", uri.toString());
+                Log.d("MainAtv-access_token", uri.getQueryParameter("access_token"));
+
+                TextView textView = findViewById(R.id.id_text);
+                textView.setText("Hello " + uri.getQueryParameter("username"));
+            }
+        }
+    }
+}
--- app/src/main/java/com/devsolutions/loginwithsteemconnect/MainActivity.java
+++ app/src/main/java/com/devsolutions/loginwithsteemconnect/MainActivity.java
@@ -1,7 +1,10 @@
 package com.devsolutions.loginwithsteemconnect;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url ="https://v2.steemconnect.com/oauth2/authorize?client_id=game.app&redirect_uri=http://www.mydeeplink.com&scope=vote,comment";
+
+                Uri webpage = Uri.parse(url);
+                Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+                startActivity(intent);
+            }
+        });
     }
 }
